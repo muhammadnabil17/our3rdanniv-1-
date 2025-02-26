19d17dd7c3776cb020bd2de56c60ee8fce8e0f82
whiskaszz <alfimukramin2018@gmail.com>
2025-02-27 01:23:52 +0700

    Cake.jsx

--- src/components/Cake.jsx
+++ src/components/Cake.jsx
@@ -8,6 +8,7 @@ function Cake() {
   // You may want to tweak these audio codes more to your liking.
   const [candlesBlownOut, setCandlesBlownOut] = useState(false);
   const [micPermissionGranted, setMicPermissionGranted] = useState(false);
+  const [handleClick]
 
   useEffect(() => {
     let audioContext;
