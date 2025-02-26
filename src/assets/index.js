01851467ef42d2d01af8c2a2864df521effb95b5
whiskaszz <alfimukramin2018@gmail.com>
2025-02-27 00:24:55 +0700

    index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,6 @@
 import bg from './bg.jpg'
 
-import picture from './picture1feli.jpg'
+import picture from './picture1feli.jpg./picture2feli.jpg./picture3feli.jpg./picture4feli.jpg'
 
 import confetti from './confetti.gif'
 
