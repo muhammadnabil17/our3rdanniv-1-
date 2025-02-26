062776fca3f431935a2ee9a1994614fa14b2163b
whiskaszz <alfimukramin2018@gmail.com>
2025-02-27 00:35:39 +0700

    index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,6 +2,12 @@ import bg from './bg.jpg'
 
 import picture from './picture1feli.jpg'
 
+import picture from './picture2feli.jpg'
+
+import picture from './picture3feli.jpg'
+
+import picture from './picture4feli.jpg'
+
 import confetti from './confetti.gif'
 
 import CakeSVG from './svg/CakeSVG'
