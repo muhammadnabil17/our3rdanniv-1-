d1b65b14857fa18d3334b75e80f4c7da1d59a5cf
whiskaszz <alfimukramin2018@gmail.com>
2025-02-27 00:48:20 +0700

    Picture.jsx

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -6,7 +6,7 @@ import SectionWrapper from './SectionWrapper';
 // Add your own images by putting them in the assets folder and import them.
 const images = [
  picture,
- picture,
+ picture1,
  picture,
  picture,
 
