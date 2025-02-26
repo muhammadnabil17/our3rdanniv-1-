e50af157a129988b5483b2c835faf9f1bd096fb7
whiskaszz <alfimukramin2018@gmail.com>
2025-02-27 01:45:37 +0700

    Cake.jsx

--- src/components/Cake.jsx
+++ src/components/Cake.jsx
@@ -31,44 +31,6 @@ function Cake() {
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1, ease: "easeOut" }}
-        >
-          <svg width="800" height="200" viewBox="0 0 400 200">
-            <defs>
-              <path id="curve" d="M50,150 Q200,50 350,150" fill="transparent" stroke="white" />
-            </defs>
-            <text fontSize="40" fill="white" textAnchor="middle">
-              <textPath href="#curve" startOffset="50%">
-                Happy Birthday!
-              </textPath>
-            </text>
-          </svg>
-          <Link to="/present" className="flex justify-center items-center">
-            <p className="absolute top-[30rem] xs:top-[36rem] s:top-[40rem] px-7 py-3 bg-customBlue text-white rounded-full hover:bg-blue-600 font-medium text-base text-center">
-              Next Page
-            </p>
-          </Link>
-        </motion.div>
-      )}
-
-      <div className="relative z-10">
-        <div className="absolute -top-48 left-1/2 transform -translate-x-1/2">
-          <div className="candle">
-            {!candlesBlownOut && (
-              <div>
-                {/* Hilangkan teks "blow" dan gunakan event klik */}
-                <div onClick={handleCandleClick}>
-                  <div className="flame"></div>
-                  <div className="flame"></div>
-                  <div className="flame"></div>
-                  <div className="flame"></div>
-                  <div className="flame"></div>
-                </div>
-              </div>
-            )}
-          </div>
-        </div>
-        <CakeSVG />
-      </div>
     </div>
   );
 }
