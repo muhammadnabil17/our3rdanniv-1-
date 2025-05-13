1a37c63e8aee691795ee4656c9901b584ea817cf
muhammadnabil17 <alian2007nabil@gmail.com>
2025-05-13 23:16:38 +0700

    Update Cake.jsx

--- src/components/Cake.jsx
+++ src/components/Cake.jsx
@@ -106,7 +106,7 @@ function Cake() {
               </defs>
               <text fontSize="40" fill="white" textAnchor="middle">
                 <textPath href="#curve" startOffset="50%">
-                  Happy Birthday!
+                  Happy Anniversaryyy!
                 </textPath>
               </text>
             </svg>
