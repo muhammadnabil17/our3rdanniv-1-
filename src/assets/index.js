1408156b479cd8a502c162864340eee59b8e5370
muhammadnabil17 <alian2007nabil@gmail.com>
2025-05-13 23:00:24 +0700

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -8,6 +8,18 @@ import picture2 from './foto3.jpg'
 
 import picture3 from './foto4.jpg'
 
+import picture4 from './foto5.jpg'
+
+import picture5 from './foto6.jpg'
+
+import picture6 from './foto7.jpg'
+
+import picture7 from './foto8.jpg'
+
+
+
+
+
 import confetti from './confetti.gif'
 
 import CakeSVG from './svg/CakeSVG'
@@ -20,6 +32,10 @@ export {
     picture1,
     picture2,
     picture3,
+    picture4,
+    picture5,
+    picture6,
+    picture7,
     confetti,
     CakeSVG
 }
